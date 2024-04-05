e7a9fed113065a9cb4f7f349a96f3e136c97fbbe
Emir Cem Gezer <emircem.gezer@gmail.com>
2024-04-05 17:55:06 +0200

    chore: fix refactor

--- src/PCA9685-Python.cpp
+++ src/PCA9685-Python.cpp
@@ -10,10 +10,10 @@ using namespace PCA9685;
 PYBIND11_MODULE(PCA9685, m) {
   m.doc() = "Driver library for PCA9685-based servo controllers.";
 
-  py::class_<PCA9685>(m, "PCA9685")
+  py::class_<PCA9685::PCA9685>(m, "PCA9685")
           .def(py::init<std::string,int>(),py::arg("device") = "/dev/i2c-1", py::arg("address") = 0x40)
-          .def("set_pwm_freq", &PCA9685::set_pwm_freq)
-          .def("set_pwm", &PCA9685::set_pwm)
-          .def("set_all_pwm", &PCA9685::set_all_pwm)
-          .def("set_pwm_ms", &PCA9685::set_pwm_ms);
+          .def("set_pwm_freq", &PCA9685::PCA9685::set_pwm_freq)
+          .def("set_pwm", &PCA9685::PCA9685::set_pwm)
+          .def("set_all_pwm", &PCA9685::PCA9685::set_all_pwm)
+          .def("set_pwm_ms", &PCA9685::PCA9685::set_pwm_ms);
 }
